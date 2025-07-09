3c15b44d287964e0aeb6afc99cdf2682d91665ae
Hally <491303100@qq.com>
2025-07-09 20:02:31 +0800

    feat: 完成了面包屑，面包屑动画的功能

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,21 +1,70 @@
 <script setup>
+// import { get } from 'core-js/core/dict';
+import { watch, ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+// 生成数组数据
+const route = useRoute()
+const store = useStore()
+const breadCrumbData = ref([])
+const linkHoverColor = ref(store.getters.cssVar.memuBg)
+const getBreadcrumb = (route) => {
+  breadCrumbData.value = route.matched.filter(
+    (item) => item.meta && item.meta.title
+  )
+  console.log(breadCrumbData, 'matched')
+}
+
+// 监听路由变化
+watch(
+  route,
+  () => {
+    getBreadcrumb(route)
+  },
+  {
+    immediate: true
+  }
+)
+const router = useRouter()
+const onLinkClick = (item) => {
+  router.push(item.path)
+}
 </script>
 <template>
-   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item>
-      <a href="/">活动管理</a>
-    </el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+  <el-breadcrumb class="breadcrumb" separator="/">
+    <transition-group name="breadcrumb">
+      <!-- 不可点击项 -->
+      <el-breadcrumb-item v-for="(item, index) in breadCrumbData" :key="index">
+        <!-- 不可点击项显示 -->
+        <span class="no-redirect" v-if="index === breadCrumbData.length - 1">{{
+          item.meta.title
+        }}</span>
+        <!--可点击项显示  -->
+        <span class="redirect" v-else @click.prevent="onLinkClick(item)">{{
+          item.meta.title
+        }}</span>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
 <style lang="scss" scoped>
-.breadcrumb{
-    display: inline-block;
-    margin-left:10px;
-    font-size:14px;
-    line-height: 50px;
+.breadcrumb {
+  display: inline-block;
+  margin-left: 10px;
+  font-size: 14px;
+  line-height: 50px;
+  .redirect {
+    color: #666;
+    font-size: 600;
+  }
+  .redirect:hover {
+    color: v-bind(linkHoverColor);
+    cursor: pointer;
+  }
+  ::v-deep .no-redirect {
+    color: #97a8be;
+    cursor: text;
+  }
 }
 </style>
